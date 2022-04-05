1a438f15b9d2a6c65cdd28db943adee42d89c5c9
Caio Biondo <caio.a.biondo@gmail.com>
2022-04-05 10:18:27 -0300

    Acer Lento

--- calc_raroc_py/views.py
+++ calc_raroc_py/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from .models import calculaRaroc
+from .models import calculaRaroc, Fluxo
 import pandas as pd
+#import numpy as np
 
 
 def index(request):
     calcraroc_df = pd.DataFrame(calculaRaroc.objects.all().values())
+    fluxo_df = pd.DataFrame(Fluxo.objects.all().values())
+    calcraroc_df['percent'] = (calcraroc_df['raroc_ref'].sum()) * 100
     context = {
         'calcraroc': calcraroc_df.to_html,
+        'fluxo': fluxo_df.to_html,
     }
     return render(request, 'index.html', context)
 
