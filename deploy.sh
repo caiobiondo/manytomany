6c1462dd94eac0ad066032a96a8ab160703ebb44
Caio Biondo <caio.a.biondo@gmail.com>
2022-04-08 20:02:37 -0300

    deploy automatizado

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+if [ "$1" == "" ]; then
+    echo "Please enter version"
+    exit 1
+fi
+
+echo "upgrading demo to v-${1}"
+
+# for private repos you can use wget with GithubToken in header
+# example --header "Authorization: token ${GITHUB_TOKEN}"
+
+wget https://github.com/caiobiondo/manytomany/archive/refs/heads/master.zip
+
+# unzip
+unzip master.zip
+cd manytomany-master/
+pip install -r requiriments.txt
+pip install drf-spectacular
+pip install allauth
+pip install django-allauth
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+
+# Copy service file, incase if there are any changes
+sudo cp demo.service /etc/systemd/system/demo.service
+# reload configurations incase if service file has changed
+sudo systemctl daemon-reload
+# restart the service
+sudo systemctl restart pythondeploy.service
+# start of VM restart
+sudo systemctl enable pythondeploy.service
